d57956506be4eb5bc1333360352ce07bbfe6a9dd
overclokk <info@overclokk.net>
2019-12-21 16:13:17 +0100

    Pages

--- src/Settings/Page.php
+++ src/Settings/Page.php
@@ -3,8 +3,161 @@ declare(strict_types=1);
 
 namespace ItalyStrap\Settings;
 
+use ItalyStrap\Config\ConfigInterface as Config;
 
+/**
+ * Class Page
+ * @package ItalyStrap\Settings
+ */
 class Page
 {
+	use ShowableTrait;
 
+	const DS 			= DIRECTORY_SEPARATOR;
+	const PAGE_TITLE	= 'page_title';
+	const MENU_TITLE	= 'menu_title';
+	const CAPABILITY	= 'capability';
+	const SLUG			= 'menu_slug';
+	const CALLBACK		= 'callback';
+	const ICON			= 'icon_url';
+	const POSITION		= 'position';
+	const VIEW			= 'view';
+	const PARENT		= 'parent';
+
+	/**
+	 * @var Config
+	 */
+	private $config;
+	private $pagenow;
+	private $sections;
+
+	/**
+	 * @var ViewPageInterface
+	 */
+	private $view;
+
+	/**
+	 * @var string
+	 */
+	private $options_group;
+
+	/**
+	 * @var string
+	 */
+	private $view_file = '';
+
+	/**
+	 * Pages constructor.
+	 * @param Config $config
+	 * @param SectionsInterface $sections
+	 * @param ViewPageInterface $view
+	 */
+	public function __construct( Config $config, SectionsInterface $sections, ViewPageInterface $view ) {
+
+		if ( isset( $_GET['page'] ) ) { // Input var okay.
+			$this->pagenow = \stripslashes( $_GET['page'] ); // Input var okay.
+		}
+
+		$this->config = $config;
+		$this->sections = $sections;
+		$this->options_group = $sections->getGroup();
+		$this->view = $view;
+		$this->view->withSections( $this->sections );
+	}
+
+	/**
+	 * Add plugin primary page in admin panel
+	 */
+	public function register() {
+
+		$config = $this->config;
+
+		$this->assertHasMinimumValueSet( $config );
+		$this->parseWithDefault( $config );
+
+		$callable = $config[ self::CALLBACK ];
+
+		if ( $config[ self::PARENT ] ) {
+
+			if ( ! $this->showOn( $config[ 'show_on' ] ) ) {
+				continue;
+			}
+
+			\add_submenu_page(
+				$config[ self::PARENT ],
+				$config[ self::PAGE_TITLE ],
+				$config[ self::MENU_TITLE ],
+				$config[ self::CAPABILITY ],
+				$config[ self::SLUG ],
+				$this->getCallable( $callable, $config )
+			);
+
+			continue;
+		}
+
+		\add_menu_page(
+			$config[ self::PAGE_TITLE ],
+			$config[ self::MENU_TITLE ],
+			$config[ self::CAPABILITY ],
+			$config[ self::SLUG ],
+			$this->getCallable( $callable, $config ),
+			$config[ self::ICON ],
+			$config[ self::POSITION ]
+		);
+	}
+
+	/**
+	 * @param array $config
+	 */
+	private function parseWithDefault( array &$config ) {
+
+		$default = [
+			'show_on'			=> true,
+			self::PAGE_TITLE	=> '',
+			self::MENU_TITLE	=> '',
+			self::CAPABILITY	=> 'manage_options',
+			self::SLUG			=> '',
+			self::CALLBACK		=> null,
+			self::ICON			=> '',
+			self::POSITION		=> null,
+			self::VIEW			=> '',
+
+			// For child pages
+			self::PARENT		=> false,
+		];
+
+		$config = \array_replace_recursive( $default, $config );
+	}
+
+	/**
+	 * @param $config
+	 */
+	private function assertHasMinimumValueSet( Config $config ) {
+		if ( ! $config->has( self::MENU_TITLE ) ) {
+			throw new \RuntimeException( \sprintf( '%s must be not empty', self::MENU_TITLE ) );
+		}
+
+		if ( ! $config->has( self::SLUG ) ) {
+			throw new \RuntimeException( \sprintf( '%s must be not empty', self::SLUG ) );
+		}
+	}
+
+	/**
+	 * The add_submenu_page callback
+	 * @param array $config
+	 */
+	private function getView( $config = [] ) {
+		$this->view->render( $config[ self::VIEW ] );
+	}
+
+	/**
+	 * @param mixed $callable
+	 * @param array $config
+	 * @return callable|\Closure
+	 */
+	private function getCallable( $callable, $config ) {
+		return \is_callable( $callable ) ? $callable : function () use ( $config ) {
+			$this->getView( $config );
+		};
+	}
 }
