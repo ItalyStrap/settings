fcd6aea5a7cdea8e84f371c7806a86ccb71d23d5
overclokk <info@overclokk.net>
2019-12-26 23:43:46 +0100

    Settings factory

--- src/Settings/SettingsFactory.php
+++ src/Settings/SettingsFactory.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace ItalyStrap\Settings;
 
+use ItalyStrap\Config\ConfigFactory;
 use ItalyStrap\Config\ConfigInterface;
+use ItalyStrap\DataParser\ParserFactory;
+use ItalyStrap\Fields\Fields;
 
 /**
  * Class SettingsFactory
@@ -13,20 +16,20 @@ class SettingsFactory
 {
 	public static function make( ConfigInterface $config, $option_name, $plugin_name = '' ) {
 
-		$options_obj = new \ItalyStrap\Settings\Options( $option_name );
+		$options_obj = new Options( $option_name );
 
 		foreach ( $config as $item ) {
-			$sections_obj = new \ItalyStrap\Settings\Sections(
-				\ItalyStrap\Config\ConfigFactory::make( $item['sections'] ),
-				new \ItalyStrap\Fields\Fields(),
-				\ItalyStrap\DataParser\ParserFactory::make( $plugin_name ),
+			$sections_obj = new Sections(
+				ConfigFactory::make( $item['sections'] ),
+				new Fields(),
+				ParserFactory::make( $plugin_name ),
 				$options_obj
 			);
 			$sections_obj->boot();
 
-			$pages_obj = new \ItalyStrap\Settings\Page(
-				\ItalyStrap\Config\ConfigFactory::make( $item['page'] ),
-				new \ItalyStrap\Settings\ViewPage(),
+			$pages_obj = new Page(
+				ConfigFactory::make( $item['page'] ),
+				new ViewPage(),
 				$sections_obj
 			);
 			$pages_obj->boot();
