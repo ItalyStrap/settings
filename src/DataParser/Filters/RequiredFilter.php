b1a075088249f21f0b67f95e28be7d986eaba853
overclokk <info@overclokk.net>
2020-01-06 15:44:56 +0100

    DataParser

---
+++ src/DataParser/Filters/RequiredFilter.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace ItalyStrap\DataParser\Filters;
+
+use ItalyStrap\DataParser\FilterableInterface;
+
+/**
+ * Class RequiredFilter
+ * @package ItalyStrap\DataParser\Filters
+ */
+class RequiredFilter implements FilterableInterface
+{
+
+	/**
+	 * @inheritDoc
+	 */
+	public function filter( $data, array $schema ) {
+		// TODO: Implement filter() method.
+	}
+}
