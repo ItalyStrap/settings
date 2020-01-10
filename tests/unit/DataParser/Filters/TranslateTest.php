8e886354a96d1e5d26c29bba2ee3b81a1f54147a
overclokk <info@overclokk.net>
2020-01-10 16:14:30 +0100

    DataParser

--- tests/unit/DataParser/Filters/TranslateTest.php
+++ tests/unit/DataParser/Filters/TranslateTest.php
@@ -3,8 +3,62 @@ declare(strict_types=1);
 
 namespace ItalyStrap\Tests;
 
+use ItalyStrap\DataParser\FilterableInterface;
+use ItalyStrap\DataParser\Filters\ThemeModFilter;
+use ItalyStrap\DataParser\Filters\TranslateFilter;
+use ItalyStrap\I18N\Translator;
 
-class TranslateTest
-{
+// phpcs:disable
+require_once 'BaseFilter.php';
+// phpcs:enable
 
+/**
+ * Class SanitizeTest
+ * @package ItalyStrap\Tests
+ */
+class TranslateTest extends BaseFilter {
+
+
+
+	/**
+	 * @var \UnitTester
+	 */
+	protected $tester;
+
+	/**
+	 * @var \Prophecy\Prophecy\ObjectProphecy
+	 */
+	private $translator;
+
+	/**
+	 * @return Translator
+	 */
+	public function getTranslator(): Translator {
+		return $this->translator->reveal();
+	}
+
+	// phpcs:ignore -- Method from Codeception
+	protected function _before() {
+		$this->translator = $this->prophesize( Translator::class );
+	}
+
+	// phpcs:ignore -- Method from Codeception
+	protected function _after() {
+	}
+
+	protected function getInstance(): FilterableInterface {
+		$sut = new TranslateFilter( $this->getTranslator() );
+		$this->assertInstanceOf( TranslateFilter::class, $sut, '' );
+		return $sut;
+	}
+
+	/**
+	 * @test
+	 */
+	public function itShouldFilter() {
+
+		$sut = $this->getInstance();
+		$value = $sut->filter( '', 'value', [ $sut::KEY => true, 'id' => 'key' ] );
+		$this->assertStringContainsString( 'value', $value, '' );
+	}
 }
